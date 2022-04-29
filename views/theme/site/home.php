97f1bfc0bc5cae81df3e0b1810d6b03c7c48aedd
Caio <bladellano@gmail.com>
2022-04-29 16:55:53 -0300

    Resumindo no front o sobre o curso

--- views/theme/site/home.php
+++ views/theme/site/home.php
@@ -151,7 +151,7 @@
                     <div data-url="<?= SITE['root'] . DS . 'cursos' . DS . $c->slug ?>" class="col-lg-4 col-md-6 content-item portfolio-item filter-<?= $c->area ?>">
                         <span><?= mb_strtoupper($c->nome) ?></span>
                         <h4><i class="bi bi-calendar4-week"></i> <?= $c->duracao ?> meses | <i class="bx bx-medal"></i> Reconhecido pelo MEC</h4>
-                        <p><?= resume($c->sobre_o_curso,50) ?></p>
+                        <p><?= resume($c->sobre_o_curso,80) ?></p>
                     </div>
 
                 <?php endforeach; ?>
